fcdd01fc04e425316287f566cacaeb766f0796b9
Richard Passos <richard.ac.passos12@gmail.com>
2024-03-13 12:39:50 -0300

    First commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,71 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface EstruturaSecao extends Schema.Component {
+  collectionName: 'components_estrutura_secaos';
+  info: {
+    displayName: 'Se\u00E7\u00E3o';
+    icon: 'layout';
+    description: '';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    title: Attribute.String & Attribute.Required;
+    subtitle: Attribute.String;
+    description: Attribute.Text;
+    image: Attribute.Media;
+    pages: Attribute.Relation<'estrutura.secao', 'oneToMany', 'api::page.page'>;
+    actions: Attribute.Component<'ui.botao', true>;
+    items: Attribute.Component<'ui.item', true>;
+  };
+}
+
+export interface SeoMetadado extends Schema.Component {
+  collectionName: 'components_seo_metadados';
+  info: {
+    displayName: 'Metadado';
+    icon: 'earth';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    image: Attribute.Media;
+  };
+}
+
+export interface UiBotao extends Schema.Component {
+  collectionName: 'components_ui_botaos';
+  info: {
+    displayName: 'A\u00E7\u00E3o';
+    icon: 'plus';
+    description: '';
+  };
+  attributes: {
+    label: Attribute.String & Attribute.Required;
+    href: Attribute.String;
+  };
+}
+
+export interface UiItem extends Schema.Component {
+  collectionName: 'components_ui_items';
+  info: {
+    displayName: 'Item';
+    icon: 'bulletList';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'estrutura.secao': EstruturaSecao;
+      'seo.metadado': SeoMetadado;
+      'ui.botao': UiBotao;
+      'ui.item': UiItem;
+    }
+  }
+}
